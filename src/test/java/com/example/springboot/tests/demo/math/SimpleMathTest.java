595856dbac97c8177d73ed95a98adfbe6ea072a0
fersilverio <ssilveriofernando45@gmail.com>
2024-07-13 18:33:26 -0300

    more assertions

--- src/test/java/com/example/springboot/tests/demo/math/SimpleMathTest.java
+++ src/test/java/com/example/springboot/tests/demo/math/SimpleMathTest.java
@@ -22,18 +22,62 @@ class SimpleMathTest {
     }
 
     @Test
-    void subtraction() {
+    void testSubtraction() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 4D;
+        double secondNumber = 1.4D;
+
+        Double actual = math.subtraction(firstNumber, secondNumber);
+
+        double expected = 2.6D;
+
+        assertEquals(expected, actual, () -> firstNumber + "-" + secondNumber + " did not produce " + expected + "!");
+        assertNotNull(actual);
+        assertNotEquals(5.4D, actual);
+
     }
 
     @Test
-    void division() {
+    void testDivision() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6D;
+        double secondNumber = 3D;
+
+        Double actual = math.division(firstNumber, secondNumber);
+
+        double expected = 2D;
+
+        assertEquals(expected, actual, () -> firstNumber + "/" + secondNumber + " did not produce " + expected + "!");
+        assertNotNull(actual);
+        assertNotEquals(5.4D, actual);
     }
 
     @Test
-    void mean() {
+    void testMean() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6D;
+        double secondNumber = 3D;
+
+        Double actual = math.mean(firstNumber, secondNumber);
+        double expected = 4.5D;
+
+        assertEquals(expected, actual, () -> "(" + firstNumber + "+" + secondNumber + ")" + " did not produce " + expected + "!");
+        assertNotNull(actual);
+        assertNotEquals(5.4D, actual);
     }
 
     @Test
-    void squareRoot() {
+    void testSquareRoot() {
+        SimpleMath math = new SimpleMath();
+        double number = 9D;
+
+
+        Double actual = math.squareRoot(number);
+        double expected = 3D;
+
+        assertEquals(expected, actual, () -> "sqrt of " + number + " did not produce " + expected + "!");
+        assertNotNull(actual);
+        assertNotEquals(5.4D, actual);
+
     }
 }
