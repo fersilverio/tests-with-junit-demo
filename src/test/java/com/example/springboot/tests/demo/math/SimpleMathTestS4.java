ff67451601e15495f572a3dbc29d43b33e9afb34
fersilverio <ssilveriofernando45@gmail.com>
2024-07-16 21:12:39 -0300

    tests with csv source and csv file source

--- src/test/java/com/example/springboot/tests/demo/math/SimpleMathTestS4.java
+++ src/test/java/com/example/springboot/tests/demo/math/SimpleMathTestS4.java
@@ -3,13 +3,15 @@ package com.example.springboot.tests.demo.math;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("Test Math Operations in SimpleMath Class")
+@DisplayName("Test Math Operations in SimpleMath Class S4")
 class SimpleMathTestS4 {
 
     SimpleMath math;
@@ -24,7 +26,13 @@ class SimpleMathTestS4 {
     @DisplayName("Test 6 / 3 = 2")
     @ParameterizedTest
     //@MethodSource("testDivisionInputParameters")
-    @MethodSource
+    //@MethodSource
+//    @CsvSource({
+//        "6.2, 2, 3.1",
+//        "71, 14, 5.07",
+//        "18.3, 3.1, 5.90"
+//    })
+    @CsvFileSource(resources = "/testDivision.csv")
     void testDivision(double firstNumber, double secondNumber, double expected) {
         Double actual = math.division(firstNumber, secondNumber);
 
@@ -34,11 +42,11 @@ class SimpleMathTestS4 {
     }
 
     //public static Stream<Arguments> testDivisionInputParameters() {
-      public static  Stream<Arguments> testDivision() {
-        return Stream.of(
-                Arguments.of(6.2D, 2D, 3.1D),
-                Arguments.of(71D, 14D, 5.07D),
-                Arguments.of(18.3D, 3.1D, 5.90D)
-        );
-    }
+//      public static  Stream<Arguments> testDivision() {
+//        return Stream.of(
+//                Arguments.of(6.2D, 2D, 3.1D),
+//                Arguments.of(71D, 14D, 5.07D),
+//                Arguments.of(18.3D, 3.1D, 5.90D)
+//        );
+//    }
 }
