a49323014eb216fc55c98b07c96f35ba81dfb665
fersilverio <ssilveriofernando45@gmail.com>
2024-07-16 21:31:33 -0300

    random sort execution tests to avoid dependencies between tests.

---
+++ src/test/java/com/example/springboot/tests/demo/MethodOrderedRandonlyTest.java
@@ -0,0 +1,29 @@
+package com.example.springboot.tests.demo;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodOrderedRandonlyTest {
+
+    @Test
+    void testA(){
+        System.out.println("Running test A");
+    }
+
+    @Test
+    void testB(){
+        System.out.println("Running test B");
+    }
+
+    @Test
+    void testC(){
+        System.out.println("Running test C");
+    }
+
+    @Test
+    void testD(){
+        System.out.println("Running test D");
+    }
+}
