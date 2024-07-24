c71c7f7a06934be80565967c65c30f6fa2387d8e
fersilverio <ssilveriofernando45@gmail.com>
2024-07-23 21:05:59 -0300

    ListTest - Mocking Exceptions

---
+++ mockito/src/test/java/com/examples/mockito/MockitoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.examples.mockito;
+
+import org.junit.jupiter.api.Test;
+
+class MockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- mockito/src/test/java/com/examples/mockito/business/ListTest.java
+++ mockito/src/test/java/com/examples/mockito/business/ListTest.java
@@ -59,4 +59,22 @@ public class ListTest {
         assertEquals("Erudio", list.get(anyInt()));
         assertEquals("Erudio", list.get(anyInt()));
     }
+
+    @Test
+    void testMockingList_When_ThrowsAnException() {
+
+        // Given / Arrange
+        var list = mock(List.class);
+
+        // If you are using argument matchers, all arguments
+        // have to be provided by matchers.
+        when(list.get(anyInt())).thenThrow(new RuntimeException("Foo Bar!!"));
+
+        // When / Act & Then / Assert
+        assertThrows(RuntimeException.class,
+                () -> {
+                    // When / Act
+                    list.get(anyInt());},
+                () -> "Should have throw an RuntimeException");
+    }
 }
